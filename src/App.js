104bf30f54218d3868861d01ce9415688378b0c5
mahlilmahee <mahlilmahee942@gmail.com>
2022-10-15 12:08:55 +0600

    map marker added

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Grid } from '@mui/material';
 import List from './Component/List/List';
 import Mymap from './Component/Map/Map';
 {/* <link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.1/dist/leaflet.css" /> */}
-
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.1/dist/leaflet.css" />
 const App= ()=>{
 
     return(
@@ -15,14 +15,14 @@ const App= ()=>{
             <CssBaseline/>
 
             <Header></Header>
-            {/* <Grid container spacing={3} style={{width: '100%'}}>
+            <Grid container spacing={3} style={{width: '100%'}}>
   <Grid item xs={12} md={4}>
   <List></List>
   </Grid>
-  <Grid item xs={12} md={4}> */}
+  <Grid item xs={12} md={4}>
     <Mymap/>
-  {/* </Grid>
-</Grid> */}
+  </Grid>
+</Grid>
         
         </>
     )
--- src/Component/Map/osm-providers.js
+++
@@ -1,6 +0,0 @@
-export default {
-    maptiler:{
-        url:"https://api.maptiler.com/maps/toner/256/{z}/{x}/{y}.png?key=G6YT4DyU9UxVgYQJwSOa",
-        attribution:'https://api.maptiler.com/maps/toner/256/tiles.json?key=G6YT4DyU9UxVgYQJwSOa'
-    }
-}
